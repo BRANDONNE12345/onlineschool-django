9b05191f700c84980736327839aec175db2ac68a
Eren-73 <alhousein73@gmail.com>
2025-03-17 10:48:49 +0000

    Dynamisation des dashbord

--- onlineschool/views.py
+++ onlineschool/views.py
@@ -1,4 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from finance.models import Payment, Salary 
+from courses.models import Course
+from users.models import User
+
+
 
 def index(request):
     return render(request, 'index.html')  # Assure-toi que 'index.html' est bien placé
@@ -7,12 +13,23 @@ def dashboard_etudiant(request):
     return render(request, 'dashboard_etudiant.html')  
 
 def dashboard_finance(request):
-    return render(request, 'dashboard_finance.html')
+    payments = Payment.objects.all()  # Récupère tous les paiements
+    salaries = Salary.objects.all()  # Récupère tous les salaires
+    return render(request, 'dashboard_finance.html', {'payments': payments, 'salaries': salaries})
 
 
 def dashboard_enseignant(request):
-    # Vous pouvez également ajouter ici les variables de contexte nécessaires (par exemple, courses, quizzes, forum_messages, etc.)
-    return render(request, 'dashboard_enseignant.html')
+    # Récupère le premier utilisateur avec le rôle "ENSEIGNANT"
+    user = User.objects.filter(role='ENSEIGNANT').first()
+    
+    if user is None:
+        # Si aucun enseignant n'est trouvé, tu peux renvoyer une erreur ou un message
+        return HttpResponse("Aucun enseignant trouvé", status=404)
+    
+    # Récupère les cours de cet enseignant
+    courses = Course.objects.filter(teacher=user)
+    return render(request, 'dashboard_enseignant.html', {'courses': courses})
+
 
 
 def course(request):
